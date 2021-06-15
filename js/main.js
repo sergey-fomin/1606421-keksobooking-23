8877e631c59c08ce59ab554a394f1a2acd47133e
sergey-fomin <ya.sergeyfomin@gmail.com>
2021-06-15 22:12:13 +0500

    добавляет функции со случайными числами

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Возвращает случайное целое число из переданного диапазона включительно
+
+function getRandomIntInclusive(min, max) {
+  if (min < 0 || max < 0 || min === max || min > max) {
+    return 'Введите два разных числа больше или равных нулю';
+  } else {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
+
+getRandomIntInclusive();
+
+// Возвращает случайное число с плавающей точкой из переданного диапазона включительно
+
+function getRandomFloatingPointNumber(min, max, decimals) {
+  if (min < 0 || max < 0 || min === max || min > max) {
+    return 'Введите два разных числа больше или равных нулю чтобы задать диапазон значений, а также количество знаков после запятой';
+  } else {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const number = Math.random() * (max - min + 1) + min;
+    return +number.toFixed(decimals);
+  }
+}
+
+getRandomFloatingPointNumber();
