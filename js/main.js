c2624e54a6689935acce608d1b211852bf550068
sergey-fomin <ya.sergeyfomin@gmail.com>
2021-07-07 19:24:10 +0500

    добавляет реализацию объекта offer

--- js/main.js
+++ js/main.js
@@ -18,12 +18,24 @@ const PLACE_PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
+const TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
 const AVATARS_COUNT = 10;
+const MIN_PLACE_PRICE = 5000;
+const MAX_PLACE_PRICE = 20000;
+const MIN_ROOMS = 1;
+const MAX_ROOMS = 20;
+const MIN_GUESTS = 1;
+const MAX_GUESTS = 10;
+const MAX_PLACE_PHOTOS = 10;
 
 // Возвращает случайное целое число из переданного диапазона включительно
 
 const getRandomIntInclusive = (min, max) => {
-  if (min < 0 || max < 0 || min === max || min > max) {
+  if (min < 0 || max < 0 || min > max) {
     return 'Введите два разных числа больше или равных нулю';
   } else {
     min = Math.ceil(min);
@@ -57,11 +69,31 @@ const makeArrayWithIncreasingNumbers = (arrayLength = 2) => {
   return newArray;
 };
 
+const getRandomElement= (someArray) => someArray[getRandomIntInclusive(0, someArray.length - 1)];
+
 const getRandomElementNoRepeat = (someArray) => {
   const randomArrayIndex = getRandomIntInclusive(0, someArray.length - 1);
   const randomArrayElement = someArray[randomArrayIndex];
   someArray.splice(randomArrayIndex, 1);
   return randomArrayElement;
+};
+
+const getFewRandomElements = (someArray, maxElements) => {
+  const counter = getRandomIntInclusive(1, maxElements);
+  const newArray = new Array(counter).fill('.');
+  for (let ind = 0; ind <= counter - 1; ind++) {
+    newArray[ind] = getRandomElement(someArray);
+  }
+  return newArray;
+};
+
+const getFewRandomElementsNoRepeat = (someArray) => {
+  const counter = getRandomIntInclusive(0, someArray.length);
+  const newArray = new Array(counter).fill('.');
+  for (let ind = 0; ind <= counter - 1; ind++) {
+    newArray[ind] = getRandomElementNoRepeat(someArray);
+  }
+  return newArray;
 };
 
 const author = {
@@ -73,46 +105,34 @@ const location = {
   lng: getRandomFloatingPointNumber(139.7, 139.8, 5),
 };
 
-console.log(location);
+const offer = {
+  title: 'Лучшее предложение для солидных котов',
+  address: `${location.lat}, ${location.lng}`,
+  price: getRandomIntInclusive(MIN_PLACE_PRICE, MAX_PLACE_PRICE),
+  type: getRandomElement(PLACE_TYPE),
+  rooms: getRandomIntInclusive(MIN_ROOMS, MAX_ROOMS),
+  guests: getRandomIntInclusive(MIN_GUESTS, MAX_GUESTS),
+  checkin: getRandomElement(TIMES),
+  checkout: getRandomElement(TIMES),
+  features: getFewRandomElementsNoRepeat(PLACE_FEATURES),
+  description: 'Уголок романтики в огромном городе',
+  photos: getFewRandomElements(PLACE_PHOTOS, MAX_PLACE_PHOTOS),
+};
+
+const createAdvert = () => {
+
+}
+// console.log(getFewRandomElements(PLACE_PHOTOS, MAX_PLACE_PHOTOS));
+// console.log(offer);
 
 
 /*
 Структура каждого объекта должна быть следующей:
 
-author, объект — описывает автора. Содержит одно поле:
-
-  avatar, строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 10. Перед однозначными числами ставится 0. Например, 01, 02...10. Адреса изображений не повторяются.
-
-
 offer, объект — содержит информацию об объявлении. Состоит из полей:
-
-  title, строка — заголовок предложения. Придумайте самостоятельно.
-
-  address, строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.lat}}, {{location.lng}}.
-
-  price, число — стоимость. Случайное целое положительное число.
-
-  type, строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
-
-  rooms, число — количество комнат. Случайное целое положительное число.
-
-  guests, число — количество гостей, которое можно разместить. Случайное целое положительное число.
-
-  checkin, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-
-  checkout, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-
-  features, массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
-
   description, строка — описание помещения. Придумайте самостоятельно.
 
   photos, массив строк — массив случайной длины из значений: https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.
-
-
-location, объект — местоположение в виде географических координат. Состоит из двух полей:
-
-  lat,
-  lng,
 
 
 const createAdvert = () => {
