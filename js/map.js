1c928a779a03dfad141d8cbc37ea1dab78b4bb84
sergey-fomin <ya.sergeyfomin@gmail.com>
2021-07-19 19:35:04 +0500

    добавляет api карты и функционал меток

--- js/map.js
+++ js/map.js
@@ -1,18 +1,102 @@
 import { enablePage } from './load.js';
+import { setPinAddress } from './form.js';
+import { newCardData, generateCustomPopup } from './offers.js';
+
+const tempOffers = newCardData;
+const MapOptions = {
+  ZOOM: 10,
+  DEFAULT_COORDS: {
+    lat: 35.6895,
+    lng: 139.692,
+  },
+  TILE: {
+    URL: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    ATTR: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+  MARKER: {
+    ICON_PATH: './img/',
+    MAIN: {
+      ICON_NAME: 'main-pin.svg',
+      ICON_SIZE: [52, 52],
+      ICON_ANCHOR: [26, 52],
+    },
+    DEFAULT: {
+      ICON_NAME: 'pin.svg',
+      ICON_SIZE: [40, 40],
+      ICON_ANCHOR: [20, 40],
+    },
+  },
+};
 
 const map = L.map('map-canvas')
   .on('load', () => {
     enablePage();
+    setPinAddress(MapOptions.DEFAULT_COORDS);
   })
   .setView(
     {
-      lat: 35.6895,
-      lng: 139.692,
+      lat: MapOptions.DEFAULT_COORDS.lat,
+      lng: MapOptions.DEFAULT_COORDS.lng,
     },
-    10,
+    MapOptions.ZOOM,
   );
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+L.tileLayer(MapOptions.TILE.URL, {
+  attribution: MapOptions.TILE.ATTR,
 }).addTo(map);
+
+const mainMarkerIcon = L.icon({
+  iconUrl: `${MapOptions.MARKER.ICON_PATH}${MapOptions.MARKER.MAIN.ICON_NAME}`,
+  iconSize: MapOptions.MARKER.MAIN.ICON_SIZE,
+  iconAnchor: MapOptions.MARKER.MAIN.ICON_ANCHOR,
+});
+const mainMarker = L.marker(
+  {
+    lat: MapOptions.DEFAULT_COORDS.lat,
+    lng: MapOptions.DEFAULT_COORDS.lng,
+  },
+  {
+    icon: mainMarkerIcon,
+    draggable: true,
+  },
+);
+
+mainMarker.addTo(map);
+mainMarker.on('move', (evt) => {
+  setPinAddress(evt.target.getLatLng());
+});
+
+const markerGroup = L.layerGroup().addTo(map);
+
+const createMarker = (elem) => {
+  const icon = L.icon({
+    iconUrl: `${MapOptions.MARKER.ICON_PATH}${MapOptions.MARKER.DEFAULT.ICON_NAME}`,
+    iconSize: MapOptions.MARKER.DEFAULT.ICON_SIZE,
+    iconAnchor: MapOptions.MARKER.DEFAULT.ICON_ANCHOR,
+  });
+
+  const marker = L.marker(elem.location, { icon });
+
+  marker.addTo(markerGroup).bindPopup(generateCustomPopup(elem), { keepInView: true });
+};
+
+tempOffers.forEach((elem) => {
+  createMarker(elem);
+});
+
+const resetMap = () => {
+  map.setView(
+    {
+      lat: MapOptions.DEFAULT_COORDS.lat,
+      lng: MapOptions.DEFAULT_COORDS.lng,
+    },
+    MapOptions.ZOOM,
+  );
+  mainMarker.setLatLng([
+    MapOptions.DEFAULT_COORDS.lat,
+    MapOptions.DEFAULT_COORDS.lng,
+  ]);
+  setPinAddress(MapOptions.DEFAULT_COORDS);
+};
+
+export { resetMap };
--- js/offers.js
+++ js/offers.js
@@ -1,10 +1,10 @@
 import { generateNearbyAdverts } from './data.js';
 
-const cardTemplate = document
+const popupTemplate = document
   .querySelector('#card')
   .content.querySelector('.popup');
-const cityMap = document.querySelector('#map-canvas');
-const offersFragment = document.createDocumentFragment();
+// const cityMap = document.querySelector('#map-canvas');
+// const offersFragment = document.createDocumentFragment();
 const newCardData = generateNearbyAdverts;
 const PLACE_TYPE = {
   palace: 'Дворец',
@@ -20,82 +20,160 @@ const isEmpty = (data) => {
   }
 };
 
-newCardData.forEach(({ author, offer }) => {
-  const newCard = cardTemplate.cloneNode(true);
+const generateCustomPopup = (elem) => {
+  const newPopupElement = popupTemplate.cloneNode(true);
 
-  newCard.querySelector('.popup__title').textContent = offer.title;
-  if (isEmpty(offer.title)) {
-    newCard.querySelector('.popup__title').classList.add('hidden');
+  newPopupElement.querySelector('.popup__title').textContent = elem.offer.title;
+  if (isEmpty(elem.offer.title)) {
+    newPopupElement.querySelector('.popup__title').classList.add('hidden');
   }
 
-  newCard.querySelector('.popup__text--address').textContent = offer.address;
-  if (isEmpty(offer.address)) {
-    newCard.querySelector('.popup__text--address').classList.add('hidden');
+  newPopupElement.querySelector('.popup__text--address').textContent = elem.offer.address;
+  if (isEmpty(elem.offer.address)) {
+    newPopupElement.querySelector('.popup__text--address').classList.add('hidden');
   }
 
-  newCard.querySelector(
+  newPopupElement.querySelector(
     '.popup__text--price',
-  ).textContent = `${offer.price} ₽/ночь`;
-  if (isEmpty(offer.price)) {
-    newCard.querySelector('.popup__text--price').classList.add('hidden');
+  ).textContent = `${elem.offer.price} ₽/ночь`;
+  if (isEmpty(elem.offer.price)) {
+    newPopupElement.querySelector('.popup__text--price').classList.add('hidden');
   }
 
-  newCard.querySelector('.popup__type').textContent = PLACE_TYPE[offer.type];
-  if (isEmpty(offer.type)) {
-    newCard.querySelector('.popup__type').classList.add('hidden');
+  newPopupElement.querySelector('.popup__type').textContent = PLACE_TYPE[elem.offer.type];
+  if (isEmpty(elem.offer.type)) {
+    newPopupElement.querySelector('.popup__type').classList.add('hidden');
   }
 
-  newCard.querySelector(
+  newPopupElement.querySelector(
     '.popup__text--capacity',
-  ).textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-  if (isEmpty(offer.guests)) {
-    newCard.querySelector('.popup__text--capacity').classList.add('hidden');
+  ).textContent = `${elem.offer.rooms} комнаты для ${elem.offer.guests} гостей`;
+  if (isEmpty(elem.offer.guests)) {
+    newPopupElement.querySelector('.popup__text--capacity').classList.add('hidden');
   }
 
-  newCard.querySelector(
+  newPopupElement.querySelector(
     '.popup__text--time',
-  ).textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-  if (isEmpty(offer.checkin) || isEmpty(offer.checkout)) {
-    newCard.querySelector('.popup__text--time').classList.add('hidden');
+  ).textContent = `Заезд после ${elem.offer.checkin}, выезд до ${elem.offer.checkout}`;
+  if (isEmpty(elem.offer.checkin) || isEmpty(elem.offer.checkout)) {
+    newPopupElement.querySelector('.popup__text--time').classList.add('hidden');
   }
 
-  const cardFeaturesList = newCard.querySelector('.popup__features');
-  const modifiers = offer.features.map((feature) => `popup__feature--${feature}`);
+  const cardFeaturesList = newPopupElement.querySelector('.popup__features');
+  const modifiers = elem.offer.features.map((feature) => `popup__feature--${feature}`);
   cardFeaturesList.querySelectorAll('.popup__feature').forEach((item) => {
     const modifier = item.classList[1];
     if (!modifiers.includes(modifier)) {
       item.remove();
     }
   });
-  if (isEmpty(offer.features)) {
-    newCard.querySelector('.popup__features').classList.add('hidden');
+  if (isEmpty(elem.offer.features)) {
+    newPopupElement.querySelector('.popup__features').classList.add('hidden');
   }
 
-  newCard.querySelector('.popup__description').textContent = offer.description;
-  if (isEmpty(offer.description)) {
-    newCard.querySelector('.popup__description').classList.add('hidden');
+  newPopupElement.querySelector('.popup__description').textContent = elem.offer.description;
+  if (isEmpty(elem.offer.description)) {
+    newPopupElement.querySelector('.popup__description').classList.add('hidden');
   }
 
-  const photosList = newCard.querySelector('.popup__photos');
+  const photosList = newPopupElement.querySelector('.popup__photos');
   const photoItem = photosList.querySelector('.popup__photo');
-  photoItem.src = offer.photos[0];
-  for (let i = 1; i < offer.photos.length; i++) {
+  photoItem.src = elem.offer.photos[0];
+  for (let i = 1; i < elem.offer.photos.length; i++) {
     const newphotoItem = photoItem.cloneNode(true);
-    newphotoItem.src = offer.photos[i];
+    newphotoItem.src = elem.offer.photos[i];
     photosList.appendChild(newphotoItem);
   }
-  if (isEmpty(offer.photos)) {
-    newCard.querySelector('.popup__photos').classList.add('hidden');
+  if (isEmpty(elem.offer.photos)) {
+    newPopupElement.querySelector('.popup__photos').classList.add('hidden');
   }
 
-  newCard.querySelector('.popup__avatar').src = author.avatar;
-  if (isEmpty(author.avatar)) {
-    newCard.querySelector('.popup__avatar').classList.add('hidden');
+  newPopupElement.querySelector('.popup__avatar').src = elem.author.avatar;
+  if (isEmpty(elem.author.avatar)) {
+    newPopupElement.querySelector('.popup__avatar').classList.add('hidden');
   }
 
-  offersFragment.appendChild(newCard);
-});
+  return newPopupElement;
+};
 
-const generateOffersOnMap = cityMap.appendChild(offersFragment);
 
-export {generateOffersOnMap};
+// newCardData.forEach((elem) => {
+
+// const newCard = cardTemplate.cloneNode(true);
+
+// newCard.querySelector('.popup__title').textContent = offer.title;
+// if (isEmpty(offer.title)) {
+//   newCard.querySelector('.popup__title').classList.add('hidden');
+// }
+
+// newCard.querySelector('.popup__text--address').textContent = offer.address;
+// if (isEmpty(offer.address)) {
+//   newCard.querySelector('.popup__text--address').classList.add('hidden');
+// }
+
+// newCard.querySelector(
+//   '.popup__text--price',
+// ).textContent = `${offer.price} ₽/ночь`;
+// if (isEmpty(offer.price)) {
+//   newCard.querySelector('.popup__text--price').classList.add('hidden');
+// }
+
+// newCard.querySelector('.popup__type').textContent = PLACE_TYPE[offer.type];
+// if (isEmpty(offer.type)) {
+//   newCard.querySelector('.popup__type').classList.add('hidden');
+// }
+
+// newCard.querySelector(
+//   '.popup__text--capacity',
+// ).textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+// if (isEmpty(offer.guests)) {
+//   newCard.querySelector('.popup__text--capacity').classList.add('hidden');
+// }
+
+// newCard.querySelector(
+//   '.popup__text--time',
+// ).textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+// if (isEmpty(offer.checkin) || isEmpty(offer.checkout)) {
+//   newCard.querySelector('.popup__text--time').classList.add('hidden');
+// }
+
+// const cardFeaturesList = newCard.querySelector('.popup__features');
+// const modifiers = offer.features.map((feature) => `popup__feature--${feature}`);
+// cardFeaturesList.querySelectorAll('.popup__feature').forEach((item) => {
+//   const modifier = item.classList[1];
+//   if (!modifiers.includes(modifier)) {
+//     item.remove();
+//   }
+// });
+// if (isEmpty(offer.features)) {
+//   newCard.querySelector('.popup__features').classList.add('hidden');
+// }
+
+// newCard.querySelector('.popup__description').textContent = offer.description;
+// if (isEmpty(offer.description)) {
+//   newCard.querySelector('.popup__description').classList.add('hidden');
+// }
+
+// const photosList = newCard.querySelector('.popup__photos');
+// const photoItem = photosList.querySelector('.popup__photo');
+// photoItem.src = offer.photos[0];
+// for (let i = 1; i < offer.photos.length; i++) {
+//   const newphotoItem = photoItem.cloneNode(true);
+//   newphotoItem.src = offer.photos[i];
+//   photosList.appendChild(newphotoItem);
+// }
+// if (isEmpty(offer.photos)) {
+//   newCard.querySelector('.popup__photos').classList.add('hidden');
+// }
+
+// newCard.querySelector('.popup__avatar').src = author.avatar;
+// if (isEmpty(author.avatar)) {
+//   newCard.querySelector('.popup__avatar').classList.add('hidden');
+// }
+
+//   offersFragment.appendChild(generateCustomPopup());
+// });
+
+// const generateOffersOnMap = cityMap.appendChild(offersFragment);
+
+export {newCardData, generateCustomPopup};
